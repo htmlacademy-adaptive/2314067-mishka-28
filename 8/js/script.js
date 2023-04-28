0552241ca877a07e742fc86ee1c6ef7d104e9e27
Keks <keks@htmlacademy.ru>
2023-04-28 17:13:42 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/script.js
@@ -0,0 +1,16 @@
+let buttonMenu = document.querySelector(".page-header__toggle");
+let dropMenu = document.querySelector(".navigation");
+let header = document.querySelector(".page-header");
+let navigation = document.querySelector(".navigation");
+
+let map = document.querySelector('.contacts');
+
+navigation.classList.remove("navigation--no-js");
+header.classList.remove("page-header--no-js");
+
+buttonMenu.addEventListener("click", function () {
+  buttonMenu.classList.toggle("page-header__toggle--clossed");
+  dropMenu.classList.toggle("navigation--open");
+})
+
+map.classList.remove('contacts--no-js');
