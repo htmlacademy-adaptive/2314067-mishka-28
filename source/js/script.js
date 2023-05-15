b61bef721a99df3a897b7b4cf43978ee48a78a0b
Alexandr Lisunov <lisunov_alexandr@mail.ru>
2023-05-15 21:37:53 +0300

    Вносит правки на замечания проверяющего

--- source/js/script.js
+++ source/js/script.js
@@ -1,12 +1,12 @@
-let buttonMenu = document.querySelector(".page-header__toggle");
-let dropMenu = document.querySelector(".navigation");
-let header = document.querySelector(".page-header");
-let navigation = document.querySelector(".navigation");
+let buttonMenu = document.querySelector('.page-header__toggle');
+let dropMenu = document.querySelector('.navigation');
+let header = document.querySelector('.page-header');
+let navigation = document.querySelector('.navigation');
 
-navigation.classList.remove("navigation--no-js");
-header.classList.remove("page-header--no-js");
+navigation.classList.remove('navigation--no-js');
+header.classList.remove('page-header--no-js');
 
-buttonMenu.addEventListener("click", function () {
-  buttonMenu.classList.toggle("page-header__toggle--clossed");
-  dropMenu.classList.toggle("navigation--open");
+buttonMenu.addEventListener('click', function () {
+  buttonMenu.classList.toggle('page-header__toggle--clossed');
+  dropMenu.classList.toggle('navigation--open');
 })
